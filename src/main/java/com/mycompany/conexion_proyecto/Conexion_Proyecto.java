31fa2d74307e2c6d3d921d1facb085c6e746a9a8
LIZETH <LIZETH@Lizz>
2024-11-29 11:48:12 -0500

    Se cargan archivos

---
+++ src/main/java/com/mycompany/conexion_proyecto/Conexion_Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conexion_proyecto;
+
+/**
+ *
+ * @author LIZETH
+ */
+public class Conexion_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
